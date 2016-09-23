ca82e279cb445ab2a6e77b3b18426e58cbba5cbb
ZerrickStaples <zerrick.staples@gmail.com>
2016-09-23 09:20:10 -0400

    Added files via upload.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace rollercoaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new instances
+            StreamReader reader = new StreamReader("..\\..\\rollercoaster.txt");
+            List<string> lines = new List<string>();
+            List<string> results = new List<string>();
+            
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                while(line != null)
+                {
+                    lines.Add(line);
+                    line=reader.ReadLine();
+                }
+            }
+
+            int length = lines.Count;
+            for (int i = 0; i <length; i++)
+            {
+                results.Add(change(lines[i]));
+            }
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        //Indexing every other letter to be capitalized.
+        public static string change (string line)
+        {
+            int length = line.Length;
+            int index = 0;
+            string result = "";
+
+            for (int i = 0; i< length; i++)
+            {
+                if(char.IsLetter(line[i]))
+                {
+                    if (index % 2==0)
+                    {
+                        result += line[i].ToString().ToUpper();
+                    }
+
+                    else
+                    {
+                        result += line[i].ToString().ToLower();
+                    }
+                    index++;
+                }
+                else
+                {
+                    result += line[i];
+                }
+
+            }
+            return result;
+        }
+    }
+}
